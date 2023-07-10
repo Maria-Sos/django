512af29420229be31fd58c208133d47cca55455c
mariaSOS <ms2145051478@gmail.com>
2023-07-09 22:43:38 -0500

    update functions

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, "hello/index.html")
 
 def greed(request, name):
     return HttpResponse(f"Hello, {name.capitalize()}!")
+
+def helloname(request, name):
+    return render(request, "hello/helloname.html", {
+        "name": name.capitalize()
+    })
