fd1bcb5b160e141c3df0877e8056383ef094cfdc
mariaSOS <ms2145051478@gmail.com>
2023-07-10 12:00:40 -0500

    Switch global task veraible to sessions

--- tasks/views.py
+++ tasks/views.py
@@ -5,15 +5,16 @@ from django.urls import reverse
 
 # Create your views here.
 
-tasks = []
-
 class NewTasksForm(forms.Form):
     task = forms.CharField(label="New Task")
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -21,7 +22,7 @@ def add(request):
         form = NewTasksForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
              return render(request, "tasks/add.html", {
