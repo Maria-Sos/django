504a33728e9f913d6a59d04d4a7f96a2220c65b2
mariaSOS <ms2145051478@gmail.com>
2023-07-10 11:49:18 -0500

    add TO Do app

---
+++ tasks/views.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+
+tasks = []
+
+class NewTasksForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTasksForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+             return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTasksForm()
+    })
